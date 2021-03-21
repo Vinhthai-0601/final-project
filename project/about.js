758003098da9f85edcfc2c947ea7f2ef5717cfa3
janechoi8 <janechoi2001@gmail.com>
2021-03-21 18:01:08 +0700

    update about

--- project/about.js
+++ project/about.js
@@ -1,17 +1,29 @@
-$(document).ready(function(){
-  let navbar = $("nav");
-  $(window).scroll(function(){
-    let height = $(window).scrollTop();
-    if(height >= 100)
-    {
-      navbar.removeClass("navbar-dark bg-dark");
-      navbar.addClass("navbar-light bg-light fixed-top");
-      navbar.css("padding","10");
-    }else {
-      navbar.removeClass("navbar-light bg-light");
-      navbar.addClass("navbar-dark bg-dark");
-      navbar.css("padding","10");
-    }
-    console.log(height);
-  })
+document.addEventListener("DOMContentLoaded", ()=>{
+  window.onscroll = function(){scrollEvent()};
 })
+
+function scrollEvent() {
+  let height = window.pageYOffset;
+  let el1 = document.querySelector("#row1");
+  let el2 = document.querySelector("#row2");
+  let el3 = document.querySelector("#row3");
+  let el4 = document.querySelector("#row4");
+  console.log(height);
+  if(height >= 200){
+    fadeIn(el1);
+  }
+  if (height >= 800) {
+    fadeIn(el2);
+  }
+  if (height >= 1200) {
+    fadeIn(el3);
+  }
+  if (height >= 1500) {
+    fadeIn(el4);
+  }
+}
+function fadeIn(el) {
+  setTimeout(function() {
+    el.style.opacity = "1";
+  }, 300);
+}
